5c574b338a91c314cf77ce18d7f7ca16b9fecdeb
Nebiou Daniel Berhane <124292845+nebioudaniel@users.noreply.github.com>
2024-03-12 18:58:50 +0300

    Added route for serving the index.html file and handling form submission.
    Created routes for serving the index.html file and processing form submissions. The '/' route serves the index.html file from the views directory, while the '/submit-form' route is responsible for handling form submissions and delegates the task to the formController.submitForm function.

--- services/clearbitService.js
+++ services/clearbitService.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const formController = require('../controllers/formController');
 
+// Route to serve the index.html file
 router.get('/', (req, res) => {
     res.sendFile('index.html', { root: __dirname + '/../views' });
 });
 
+// Route to handle form submission
 router.post('/submit-form', formController.submitForm);
 
 module.exports = router;
